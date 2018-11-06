ec699ea12dda7809874047576f54812cc3d80fa4
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-06 17:15:38 -0500

    updated welcome section and tiles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    let skillsShow = document.querySelector(`#skills-show`);
-    let hobbiesHide = document.querySelector(`#hobbies-hide`);
+    const skillsShow = document.querySelector(`#skills-show`);
+    const hobbiesShow = document.querySelector(`#hobbies-show`);
+    const skillsHide = document.querySelector(`#skills-hide`);
+    const hobbiesHide = document.querySelector(`#hobbies-hide`);
     let hobbyText = document.querySelector(`#hobby-text`);
     let skillsText = document.querySelector(`#skills-span`);
 
     skillsShow.addEventListener("click",function(){
-        skillsShow.innerHTML = `Skills: `;
         skillsText.classList.toggle("hide");
-        let arrowLeft = document.createElement("i");
-        arrowLeft.classList.add("fa","fa-chevron-circle-left");
-        arrowLeft.setAttribute("id","skills-hide");
-        skillsText.append(arrowLeft);
+        skillsShow.classList.toggle("hide");
+
     });
 
-
+    skillsHide.addEventListener("click",function(){
+        skillsText.classList.toggle("hide");
+        skillsShow.classList.toggle("hide");
+    })
 
     
-    hobbiesHide.addEventListener("click",function(){
+    hobbiesShow.addEventListener("click",function(){
+        hobbiesShow.classList.toggle("hide");
         hobbyText.classList.toggle("hide");
     });
 
+    hobbiesHide.addEventListener("click",function(){
+        hobbiesShow.classList.toggle("hide");
+        hobbyText.classList.toggle("hide");
+    })
 
   });
