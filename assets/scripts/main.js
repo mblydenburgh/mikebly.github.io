f5e0520a42cfb77db7f242075d94969e2f8b97cd
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-13 22:32:24 -0500

    begin to refactor to dynamically generate content

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() { 
+let projects = [
+    {
+        title: "Munchies Metropolis",
+        url: "https://pgould94.github.io/Munchies_Metropolis",
+        imgUrl: "../assets/images/munchie.jpg"
+    },
+    {
+        title:"Pupperbase (Angular JS)",
+        url:"https://mikebly.github.io/pupperbase",
+        imgUrl:"../assets/images/pupper.jpg"
+    },
+    
+]
+
+document.addEventListener("DOMContentLoaded", function () {
     const skillsShow = document.querySelector(`#skills-show`);
     const hobbiesShow = document.querySelector(`#hobbies-show`);
     const skillsHide = document.querySelector(`#skills-hide`);
@@ -6,26 +20,28 @@ document.addEventListener("DOMContentLoaded", function() {
     let hobbyText = document.querySelector(`#hobby-text`);
     let skillsText = document.querySelector(`#skills-span`);
 
-    skillsShow.addEventListener("click",function(){
+
+
+
+    skillsShow.addEventListener("click", function () {
         skillsText.classList.toggle("hide");
         skillsShow.classList.toggle("hide");
-
     });
 
-    skillsHide.addEventListener("click",function(){
+    skillsHide.addEventListener("click", function () {
         skillsText.classList.toggle("hide");
         skillsShow.classList.toggle("hide");
-    })
+    });
 
-    
-    hobbiesShow.addEventListener("click",function(){
+
+    hobbiesShow.addEventListener("click", function () {
         hobbiesShow.classList.toggle("hide");
         hobbyText.classList.toggle("hide");
     });
 
-    hobbiesHide.addEventListener("click",function(){
+    hobbiesHide.addEventListener("click", function () {
         hobbiesShow.classList.toggle("hide");
         hobbyText.classList.toggle("hide");
-    })
+    });
 
-  });
+});
