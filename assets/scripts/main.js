8782c6c8b96ed06925270cd56043130b2e85f578
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-16 18:00:40 -0500

    projects dynamically created

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,37 +2,37 @@ let projects = [
     {
         title: "Munchies Metropolis",
         url: "https://pgould94.github.io/Munchies_Metropolis",
-        imgUrl: "../assets/images/munchie.jpg"
+        imgUrl: "./assets/images/munchie.jpg"
     },
     {
         title: "Pupperbase (Angular JS)",
         url: "https://mikebly.github.io/pupperbase",
-        imgUrl: "../assets/images/pupper.jpg"
+        imgUrl: "./assets/images/pupper.jpg"
     },
     {
         title: "Rock Paper Scissors (Cross Browser w/ Firebase)",
         url: "https://mikebly.github.io/RPS-MP",
-        imgUrl: "../assets/images/rps.jpg"
+        imgUrl: "./assets/images/rps.jpg"
     },
     {
         title: "Trivia",
         url: "https://mikebly.github.io/trivia",
-        imgUrl: "../assets/images/triva.jpg"
+        imgUrl: "assets/images/trivia.jpg"
     },
     {
         title: "Giphy Goodness",
         url: "https://mikebly.github.io/giphy-goodness",
-        imgUrl: "../assets/images/giphy.jpg"
+        imgUrl: "./assets/images/giphy.jpg"
     },
     {
         title: "TSA Luggage Analysis (Angular JS)",
         url: "https://mikebly.github.io/tsa-luggage-analysis",
-        imgUrl: "../assets/images/claims.jpg"
+        imgUrl: "assets/images/claims.jpg"
     },
     {
         title: "Star Wars Battle",
         url: "https://mikebly.github.io/unit-4-game",
-        imgUrl: "../assets/images/starwars.jpg"
+        imgUrl: "./assets/images/starwars.jpg"
     },
     {
         title: "Color Game",
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const hobbiesShow = document.querySelector(`#hobbies-show`);
     const skillsHide = document.querySelector(`#skills-hide`);
     const hobbiesHide = document.querySelector(`#hobbies-hide`);
-    const projectsDiv = document.querySelector(`#projects-container`);
+    const projectsDiv = document.querySelector(`#projects`);
     let hobbyText = document.querySelector(`#hobby-text`);
     let skillsText = document.querySelector(`#skills-span`);
 
@@ -93,13 +93,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function makeCard(project) {
         let newProject = document.createElement('a');
-        newProject.classList.add('project-tile')
-        newProject.innerHTML(
+        newProject.classList.add('project-tile');
+        newProject.setAttribute("href",project.url);
+        newProject.innerHTML =
             `
-            <img src="./assets/images/munchie.jpg" alt="Munchies Metropolis">
-            <p class="project-title">Munchies Metropolis</p>
+            <img src="${project.imgUrl}" alt="${project.title}">
+            <p class="project-title">${project.title}</p>
+            <a href="">See the code!</>
             `
-        );
+        ;
         projectsDiv.appendChild(newProject);
     }
 
